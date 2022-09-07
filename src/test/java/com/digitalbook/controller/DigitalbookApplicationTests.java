bd84019bf5d59f042a457555fc91d1ca6398d426
unknown <cogjava3191@iiht.tech>
2022-09-07 14:51:06 +0530

    created project

---
+++ src/test/java/com/digitalbook/controller/DigitalbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalbook.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
